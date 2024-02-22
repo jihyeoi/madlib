a483f24745542c8d401e0a4dfbc0c66bb63aa8b5
JiHye Yoon <yoonjihyeoi@gmail.com>
2024-02-21 17:04:37 -0800

    added app.py

--- app.py
+++ app.py
@@ -7,3 +7,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/questions/')
+def questions():
+    prompts = request.args['noun']
+
+    return render_template("questions.html",
+                           prompts=noun)
+
+
+
+
+    silly_story = Story(
+    ["place", "noun", "verb", "adjective", "plural_noun"],
+
+)
+
+
+
